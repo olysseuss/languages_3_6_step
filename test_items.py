54a3d7d5dc8306ae68f6129b8c3a878049432a0a
Olysseus <olysseus@mail.ru>
2022-06-01 02:57:06 +0300

    done

---
+++ test_items.py
@@ -0,0 +1,14 @@
+from selenium.webdriver.common.by import By
+
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+
+def test_check_the_shopping_cart_button(browser):
+    browser.get(link)
+    bas_text = browser.find_element(By.CSS_SELECTOR, value=".btn-lg").text
+    print(bas_text)
+    assert "Ajouter au panier" == bas_text, "Texts do not match"
+
+
+# pytest -s -v --language=fr test_items.py
