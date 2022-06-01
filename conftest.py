4170dcfb5e0c407d8987213ed0d7b410c6788462
Olysseus <olysseus@mail.ru>
2022-06-01 03:01:27 +0300

    done finally

--- conftest.py
+++ conftest.py
@@ -5,7 +5,7 @@ from selenium.webdriver.chrome.options import Options
 
 def pytest_addoption(parser):
    parser.addoption('--language', action='store', default="en",
-                     help="Choose language: en or ru")
+                     help="Choose language")
 
 
 @pytest.fixture(scope="function")
