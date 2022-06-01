a2399fbc4012791d3f80b3200361701989c74c8d
Olysseus <olysseus@mail.ru>
2022-06-01 03:19:58 +0300

    done finally

--- test_items.py
+++ test_items.py
@@ -8,7 +8,7 @@ def test_check_the_shopping_cart_button(browser):
     browser.get(link)
     bas_text = browser.find_element(By.CSS_SELECTOR, value=".btn-lg").text
     print(bas_text)
-    assert "Ajouter au panier" == bas_text, "Texts do not match"
+    assert bas_text, "Texts do not match"
 
 
 # pytest -s -v --language=fr test_items.py
